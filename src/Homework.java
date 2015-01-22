3a30e130a2ae3c83ce5580521176c4764d2d1bea
Ross O'Reilly <oreillyross@gmail.com>
2015-01-23 00:19:54 +0100

    Added POJO persistence for MongoDB

--- src/Homework.java
+++ src/Homework.java
@@ -1,7 +1,9 @@
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.bson.types.BasicBSONList;
+import org.bson.types.ObjectId;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
@@ -11,7 +13,70 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+import org.mongodb.morphia.annotations.Embedded;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.query.Query;
+
+@Entity
+class Student {
+	
+	@Id private ObjectId id;
+	private String name;
+	
+	public ObjectId getId() {
+		return id;
+	}
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Score getScores() {
+		return scores;
+	}
+	public void setScores(Score scores) {
+		this.scores = scores;
+	}
+	private Score scores;
+	
+}
+
+@Embedded
+class Score {
+	private String type;
+	private long score;
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public long getScore() {
+		return score;
+	}
+	
+	public void setScore(long score) {
+		this.score = score;
+	}
+	
+	@Override
+	public String toString() {
+		return type + ": " + score;
+	}
+}
+
 public class Homework {
+
 
 	public static void main(String[] args) {
 		MongoClient client = null;
@@ -26,18 +91,16 @@ public class Homework {
 		long count = students.count();
 		System.out.println(count);
 		
-		DBObject query = new BasicDBObject(); 
-		DBCursor cursor = students.find(query);
-		BasicDBList scores = new BasicDBList();
-				
-		try {
-			while (cursor.hasNext()) {
-			    DBObject student = cursor.next();
-				System.out.println(student);
-			}
-		} finally {
-			cursor.close();
-		}
+		Morphia morphia = new Morphia();
+		morphia.map(Student.class).map(Score.class);
+		Datastore ds = morphia.createDatastore(client, "school");
+		
+		Query query = ds.createQuery(Score.class).filter("type", "homework");
+		Score score = (Score) query.get();
+		System.out.println(score);
+		
+		
+		
 	}
 
 }
